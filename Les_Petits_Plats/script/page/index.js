b0b6b14a1078d2029d0a3f8e5caf03ba0c4d28f2
Madeline <grammaticomadeline@hotmail.fr>
2022-05-27 16:27:56 +0200

    fix(app) : display all ingredients

--- Les_Petits_Plats/script/page/index.js
+++ Les_Petits_Plats/script/page/index.js
@@ -4,9 +4,12 @@ const searchRecipe = document.querySelector('.search__recipe');
 displayCard(recipes);
 searchRecipe.addEventListener('input', findMatches);
 
+function stringifyIngredient(ingredient, quantity, unit) {
+    return `${ingredient} : ${quantity} ${unit}`
+}
+
 function displayCard(data) {
     const cardContainer = document.querySelector("main");
-    console.log(cardContainer);
     cardContainer.innerHTML = "";
     data.forEach((recipe) => {
         const card = document.createElement('article');
@@ -15,10 +18,18 @@ function displayCard(data) {
                             <img src="" alt=""></img>
                             <span class="card__title">${recipe.name}</span>
                             <span class="card__duration">${recipe.time} min</span>
-                            <span class="card__ingredients">${recipe.ingredients[0].ingredient}</span>
+                            <div class="card__ingredients"></div>
                             <span class="card__recipe">${recipe.description}</span>
                         </a>`;
         cardContainer.appendChild(card);
+        const ingredientWrapper = cardContainer.querySelector(".card__ingredients")
+        recipe.ingredients.forEach((object) => {
+            const ingredientContainer = document.createElement("span")
+            ingredientContainer.classList = "card__ingredient__span"
+            const inner = stringifyIngredient(object.ingredient, object.quantity, object.unit)
+            ingredientContainer.innerHTML = inner
+            ingredientWrapper.appendChild(ingredientContainer);
+        })
     })
 }
 
@@ -28,7 +39,6 @@ function findMatches(e) {
         displayCard(recipes);
     } else if (e.currentTarget.classList[0] === "search__recipe" && e.currentTarget.value.length >= 3) {
         recipes.forEach((recipe)=> {
-            
             if (recipe.name.toLowerCase().indexOf(e.currentTarget.value.toLowerCase()) !== -1) {
                 result.push(recipe)
             }
