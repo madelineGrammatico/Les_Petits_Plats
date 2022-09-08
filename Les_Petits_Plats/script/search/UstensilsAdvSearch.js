59a3a6e6515960e94553b720155a78db07b9d3be
Madeline <grammaticomadeline@hotmail.fr>
2022-09-08 19:48:28 +0200

    fix(app) : clean code

--- Les_Petits_Plats/script/search/UstensilsAdvSearch.js
+++ Les_Petits_Plats/script/search/UstensilsAdvSearch.js
@@ -1,10 +1,4 @@
-// import AvencedSearch from "./AvencedSeach.js";
-// import tagFactory from "../factory/tagFactory.js";
-
 export default class UstensilsAdvSearch {
-    constructor () {
-      
-    }
     addSearchUstensils(options) {
         const searchsTagTab = new Set()
         options.results.forEach((recipe) => {
@@ -12,8 +6,7 @@ export default class UstensilsAdvSearch {
                 searchsTagTab.add(ustensil.toLowerCase())
             })
         })
+        
         return searchsTagTab
     }
-  
-    
 }
--- Les_Petits_Plats/script/tag/Tag.js
+++ Les_Petits_Plats/script/tag/Tag.js
@@ -1,18 +1,11 @@
-// import GlobalSearch from "../globalSearch/GlobalSearch.js";
 
 export default class Tag {
     constructor() {
-        // super(recipes)
         this.container = document.querySelector('.tag__container')
         this.cardContainer = document.querySelector("main");
         this.searchTab = ""
     }
-    // clearContainer() {
-    //     this.cardContainer.innerHTML = "";
-    // }
-    // #stringifyIngredients(ingredient, quantity = "", unit="") {
-    //     return `${ingredient} : ${quantity} ${unit}`
-    // }
+   
     createTag(value) {
         const tag = document.createElement("span")
         tag.classList.add("activeTag")
@@ -22,89 +15,4 @@ export default class Tag {
 
         return tag
     }
-
-    // removeTag(e, options) {
-    //     let tag
-    //     (e.target.classList[0] === "far" ) ? tag = e.target.parentNode : tag = e.target
-    //     console.log(tag.classList[1])
-    //     options.results = this.recipes
-    //     switch(tag.classList[1]) {
-    //         case 'tag--ingredient' :
-    //             options.ingredientsTagTab.delete(tag.textContent)
-    //         break
-    //         case 'tag--ustensil' :
-    //             options.ustensilsTagTab.delete(tag.textContent)
-    //         break
-    //         case 'tag--appliance' :
-    //             options.appliancesTagTab.delete(tag.textContent)
-    //         break
-    //     }
-    //     console.log(options)
-    //     this.container.removeChild(tag)
-    //     this.ultimateMatchesRecipes(options)
-    // }
-
-    // matchsRecipes(options, searchType, value = options.input) {
-    //     console.log(value)
-    //     const results = []
-    //     const valueRegex = new RegExp(value.toLowerCase())
-    //     switch(searchType) {
-    //         case 'ingredient' : 
-    //             options.results.forEach((recipe) => {
-    //                 recipe.ingredients.forEach((item) => { 
-    //                     if (valueRegex.test(item.ingredient.toLowerCase())) {
-    //                         results.push(recipe)
-    //                     }
-    //                 })  
-    //             })
-    //         break
-    //         case 'ustensil' : 
-    //             options.results.forEach((recipe) => {
-    //                 recipe.ustensils.forEach((item) => { 
-    //                     if (valueRegex.test(item.toLowerCase())) {
-    //                         results.push(recipe)
-    //                     }
-    //                 })  
-    //             })
-    //         break
-    //         case 'appliance' :
-    //             options.results.forEach((recipe) => {
-    //                 if (valueRegex.test(recipe.appliance.toLowerCase())) {
-    //                     results.push(recipe)
-    //                 }
-    //             })
-    //         break
-    //     }
-    //     options.results = results
-    //     this.displayRecipes(options.results)
-    //     return options
-    // }
-
-    // displayRecipes(results) {
-    //     this.clearContainer()
-    //     this.cardContainer.innerHTML = "";
-    //     results.forEach((recipe) => {
-    //         const card = document.createElement('article');
-    //         card.classList.add('card__Recipes');
-    //         card.innerHTML = `<a href="">
-    //                             <img src="" alt=""></img>
-    //                             <div class="card__data">
-    //                                 <span class="card__title">${recipe.name}</span>
-    //                                 <span class="card__duration"><i class="far fa-clock"></i> ${recipe.time} min</span>
-    //                                 <div class="card__ingredients"></div>
-    //                                 <span class="card__recipe">${recipe.description}</span>
-    //                             </div>
-    //                         </a>`;
-    //         this.cardContainer.appendChild(card);
-    //         const divContainer = card.querySelector(".card__ingredients")
-            
-    //         recipe.ingredients.forEach((object) => {
-    //             const inner = this.#stringifyIngredients(object.ingredient, object.quantity, object.unit)
-    //             const containerIngredients = document.createElement('span')
-    //             containerIngredients.classList.add("ingedient__span")
-    //             containerIngredients.innerHTML = inner
-    //             divContainer.appendChild(containerIngredients)
-    //         })
-    //     })
-    // }
 }
